b12f8bfea839255a29d4cd894cb11db1b0e8fe2f
Kristian Colville <kristiancolville1@gmail.com>
2026-06-27 09:06:15 +0100

    Added routing and live data from API.

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -15,6 +15,7 @@ export interface BaseMovieProps {
     revenue: number;
     vote_count: number;
     favourite?: boolean;
+    genre_ids?: number[];
 }
 
 export interface BaseMovieListProps {
@@ -48,3 +49,4 @@ export interface MoviePageProps {
     images: MovieImage[];
 }
 
+export type FilterOption = "title" | "genre";
