b16c7c0ac6c126e35dccb9f02cf5cf6a20d723ff
Kristian Colville <kristiancolville1@gmail.com>
2026-06-28 10:24:15 +0100

    Full review page.

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
+import MovieReviewPage from "./pages/movieReviewPage";
+
 
 const App = () => {
     return (
@@ -22,6 +24,7 @@ const App = () => {
                     element={<FavouriteMoviesPage />}
                 />
                 <Route path="/movies/:id" element={<MoviePage />} />
+                <Route path="/reviews/:id" element={<MovieReviewPage />} />
                 <Route path="/" element={<HomePage />} />
                 <Route path="*" element={<Navigate to="/" />} />
             </Routes>
