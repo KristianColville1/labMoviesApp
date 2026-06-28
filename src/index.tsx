5d32f962d575e3bb228d95b332f2d9118b038910
Kristian Colville <kristiancolville1@gmail.com>
2026-06-28 11:01:01 +0100

    Refactor: Favourite movie list now stored in a context.

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,8 @@ import SiteHeader from "./components/siteHeader";
 import UpcomingMoviesPage from "./pages/upcomingMoviesPage"; // NEW
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
+import MoviesContextProvider from "./contexts/moviesContext";
+
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -24,19 +26,27 @@ const queryClient = new QueryClient({
 const App = () => {
     return (
         <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-            <SiteHeader />
-            <Routes>
-                <Route
-                    path="/movies/favourites"
-                    element={<FavouriteMoviesPage />}
-                />
-                <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
-                <Route path="/movies/:id" element={<MoviePage />} />
-                <Route path="/reviews/:id" element={<MovieReviewPage />} />
-                <Route path="/" element={<HomePage />} />
-                <Route path="*" element={<Navigate to="/" />} />
-            </Routes>
+            <BrowserRouter>
+                <SiteHeader />
+                <MoviesContextProvider>
+                    <Routes>
+                        <Route
+                            path="/movies/favourites"
+                            element={<FavouriteMoviesPage />}
+                        />
+                        <Route
+                            path="/movies/upcoming"
+                            element={<UpcomingMoviesPage />}
+                        />
+                        <Route path="/movies/:id" element={<MoviePage />} />
+                        <Route
+                            path="/reviews/:id"
+                            element={<MovieReviewPage />}
+                        />
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="*" element={<Navigate to="/" />} />
+                    </Routes>
+                </MoviesContextProvider>
             </BrowserRouter>
             <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
