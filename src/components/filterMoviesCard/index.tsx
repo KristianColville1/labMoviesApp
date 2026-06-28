dea45880f1b08a405bf21754428d33e88cbfd784
Kristian Colville <kristiancolville1@gmail.com>
2026-06-28 09:21:11 +0100

    Added filtering of movie list.

--- src/components/filterMoviesCard/index.tsx
+++ src/components/filterMoviesCard/index.tsx
@@ -26,11 +26,12 @@ const styles = {
 };
 
 interface FilterMoviesCardProps {
+    onUserInput: (f: FilterOption, s: string) => void; // Add this line
     titleFilter: string;
     genreFilter: string;
 }
 
-const FilterMoviesCard: React.FC<FilterMoviesCardProps> = ({ titleFilter, genreFilter }) => {
+const FilterMoviesCard: React.FC<FilterMoviesCardProps> = ({ onUserInput, titleFilter, genreFilter }) => {
   const [genres, setGenres] = useState([{ id: "0", name: "All" }]);
 
   useEffect(() => {
@@ -53,7 +54,7 @@ const FilterMoviesCard: React.FC<FilterMoviesCardProps> = ({ titleFilter, genreF
       value: string,
   ) => {
       e.preventDefault();
-      // Completed later
+      onUserInput(type, value);
   };
 
   const handleTextChange = (e: ChangeEvent<HTMLInputElement>) => {
